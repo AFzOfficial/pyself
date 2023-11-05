6120c77528fb4beb072fa33f40bbedea59b8185f
AFzOfficial <abol.mortal50@gmail.com>
2023-11-05 18:45:07 +0330

    fix db

--- db.py
+++ db.py
@@ -1,10 +1,9 @@
 import string
-from collections import namedtuple
 
-
-Data = namedtuple('Data', ['time', 'photo', 'action', 'status', 'font'])
-
-font = '⁰¹²³⁴⁵⁶⁷⁸⁹'
-
-profile = Data(time=False, photo=False, action=False,
-               status=False, font=str.maketrans(string.digits, font))
+profile = {
+    "time": False,
+    "photo": False,
+    "status": False,
+    "action": False,
+    "font": str.maketrans(string.digits, '⁰¹²³⁴⁵⁶⁷⁸⁹'),
+}
